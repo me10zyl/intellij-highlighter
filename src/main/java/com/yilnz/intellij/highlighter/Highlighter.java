8a28d5b7f052f297b807ca9d242c91ae880eeccf
zyl <me10zyl@qq.com>
2019-12-13 11:05:35 +0800

    fix bugs

--- src/main/java/com/yilnz/intellij/highlighter/Highlighter.java
+++ src/main/java/com/yilnz/intellij/highlighter/Highlighter.java
@@ -5,18 +5,20 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
 
+import javax.lang.model.element.Element;
 import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Consumer;
 
 public class Highlighter implements Annotator {
 
-    private Set<PsiIdentifier> identifierList = new HashSet<>();
-    private int i = 1;
+    final static Key<Set<PsiIdentifier>> k = new Key<>("localvariables");
 
     public void traverse(PsiElement psiElement, AnnotationHolder holder, Color bg, Set<PsiIdentifier> identifierList) {
         final PsiElement[] children = psiElement.getChildren();
@@ -53,22 +55,58 @@ public class Highlighter implements Annotator {
         }
     }
 
+    private void walk(PsiElement element, Consumer<PsiElement> child){
+        final PsiElement[] children = element.getChildren();
+        for (PsiElement psiElement : children) {
+            child.accept(element);
+            walk(psiElement, child);
+        }
+    }
+
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-        System.out.println(i++);
         highlight(element, holder);
     }
 
     private void highlight(@NotNull PsiElement element, @NotNull AnnotationHolder holder){
         final EditorColorsScheme globalScheme = EditorColorsManager.getInstance().getGlobalScheme();
         final Color bg = globalScheme.getDefaultBackground();
+        final PsiManager manager = element.getManager();
+        Set<PsiIdentifier> identifierList = new HashSet<>();
+        final Set<PsiIdentifier> userData = manager.getUserData(k);
+        if (userData != null) {
+            identifierList = userData;
+        }else{
+            manager.putUserData(k, identifierList);
+        }
         //Get LocalVariableList;
-
-        final PsiElement parent = null;
-
+       /* PsiElement parent = null;
         do {
-
-        } while (parent != null && parent instanceof PsiJavaFile);
+            parent = element.getParent();
+            if (parent instanceof PsiMethod ) {
+                psiMethods.add((PsiMethod) parent);
+                walk(parent, c -> {
+                    if (c instanceof PsiDeclarationStatement) {
+                        PsiIdentifier identifier = null;
+                        A:
+                        for (PsiElement child : c.getChildren()) {
+                            if (child instanceof PsiLocalVariable) {
+                                final PsiElement[] children = child.getChildren();
+                                for (PsiElement psiElement : children) {
+                                    if (psiElement instanceof PsiIdentifier) {
+                                        identifier = (PsiIdentifier) psiElement;
+                                        break A;
+                                    }
+                                }
+                            }
+                        }
+                        if (identifier != null && !identifier.textMatches(identifier.getText())) {
+                            identifierList.add(identifier);
+                        }
+                    }
+                });
+            }
+        } while (parent != null && parent instanceof PsiJavaFile);*/
 
         if (element instanceof PsiDeclarationStatement) {
             PsiIdentifier identifier = null;
@@ -88,7 +126,9 @@ public class Highlighter implements Annotator {
                 identifierList.add(identifier);
             }
         }
-        //Add Color to LocalVariable and Its references
+
+
+        /*//Add Color to LocalVariable and Its references
         if (element instanceof PsiIdentifier) {
             boolean flag = false;
             if (element.getParent() instanceof PsiReferenceExpression) {
@@ -114,10 +154,10 @@ public class Highlighter implements Annotator {
                 Annotation annotation = holder.createInfoAnnotation(range, null);
                 annotation.setEnforcedTextAttributes(Colors.getRanColor(element.getText(), bg));
             }
-        }
-        
-       /* if (element instanceof PsiLocalVariable || element instanceof PsiExpressionStatement) {
-            traverse(element, holder, bg, identifierList);
         }*/
+        
+        if (element instanceof PsiLocalVariable || element instanceof PsiExpressionStatement) {
+            traverse(element, holder, bg, identifierList);
+        }
     }
 }
--- src/main/java/com/yilnz/intellij/highlighter/HighlighterFactory.java
+++ src/main/java/com/yilnz/intellij/highlighter/HighlighterFactory.java
@@ -4,9 +4,14 @@ import com.intellij.openapi.extensions.ExtensionFactory;
 
 public class HighlighterFactory implements ExtensionFactory {
 
+	private Highlighter highlighter;
+
 	@Override
 	public Object createInstance(String s, String s1) {
-		return new Highlighter();
+		if (highlighter == null) {
+			highlighter = new Highlighter();
+		}
+		return highlighter;
 	}
 }
 
